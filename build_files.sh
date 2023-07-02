28bd32deba32e6871aa2795f267950ac035b2ad8
Vikash Sharma <122241918+vikash-co@users.noreply.github.com>
2023-07-02 12:46:39 +0530

    Update build_files.sh
    remove unnecessary

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
-=======
-echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
->>>>>>> 929af51dbcc04f3cc0c76439b5db403ef4bb9d6e
